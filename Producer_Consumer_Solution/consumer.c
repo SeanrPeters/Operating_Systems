404ae136bf7bd8e0fc95a8246301ef79d55fe039
SeanrPeters <58159948+SeanrPeters@users.noreply.github.com>
2022-04-07 20:31:37 -0400

    Create consumer.c

---
+++ Producer_Consumer_Solution/consumer.c
@@ -0,0 +1,35 @@
+include "data.h"
+#include <iostream>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+
+int main()
+{
+        sleep(.1000);
+        int shared = shm_open("/sharedVal", O_RDWR, 0);
+        shm_unlink("/sharedVal");
+        struct shm *s = (struct shm *)mmap(NULL, sizeof(*s), PROT_READ | PROT_WRITE, MAP_SHA$
+
+     
+        for(int i = 0; i < 5; i++)
+                {
+                sem_wait(&s -> isFull);
+                sem_wait(&s -> sem);
+
+                s -> buf[s -> con] = rand()%5+1;
+
+                std::cout << "Consumming Table..." << std::endl;
+                std::cout << "Table[" << s -> con << "] has  " << s -> buf[s -> con] << "now" $
+
+                s -> buf[s -> con] = -1;
+                s -> con = (s -> con +1 ) % 2;
+
+                sem_post(&s -> isEmpty);
+                sem_post(&s -> sem);
+                }
+    return 0;
+}
+
+
